e703559c4f98438e8d8b8b0cc2c7c15dcf7bcfb4
Amiri Gonzalez <amiri.gonzalez@gmail.com>
2016-04-11 20:04:45 -0700

    created exercise.json file

--- class1/exercise6.py
+++ class1/exercise6.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env/python
 
-
+import json
 import yaml
-
+from pprint import pprint as pp
 
 list1 = ['strings','integers','booleans','floats']
 list1.append({})
@@ -11,5 +11,22 @@ list1[-1]['Key2'] = 'Value2'
 
 with open('exercise6.yml','w') as f:
     f.write(yaml.dump(list1,default_flow_style=False))
+
+with open('exercise6.yml','r') as a:
+   newlist = yaml.load(a)
+
+pp(newlist)
+
+
+jlist = jason.dumps(list1)
+
+
+with open('exercise6.json','w') as fhandle:
+    jason.dump(jlist,fhandle)
+
+with open('exercise6.json','r') as fhandle:
+    new_jlist = jason.load(fhandle)
+
+pp(jlist)
 
 

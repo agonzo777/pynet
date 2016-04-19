2964b13ac50f7941ddbe8a5bf0f822547125bd3f
Amiri Gonzalez <amiri.gonzalez@gmail.com>
2016-04-19 08:33:30 -0700

    Finished the configuration for exercise2 and added the done.txt since I have completed class 2 homework

--- Class2/exercise2.py
+++ Class2/exercise2.py
@@ -4,22 +4,27 @@
 import telnetlib
 import time
 
-username = 'amiri'
-password = '1234'
-ip_add = '1.2.3.4'
-telnet_port = 8124
+username = 'pyclass'
+password = '88newclass'
+ip_add = '50.76.53.27'
+telnet_port = 23
 telnet_to = 6
 
 telnet_handle = telnetlib.Telnet(ip_add,telnet_port,telnet_to)
-telnet_handle.read_until("username:", telnet_to)
-telnet_handle.write(username,telnet_to)
-telnet_handle.read_until('password:',telnet_to)
-telnet_handle.write(password,telnet_to)
-
-telnet_handle.read_until('#',telnet_to)
-telnet_handle.write('term len 0',telnet_to)
-time.sleep(3)
-telnet_handle.write('show ip int brief',telnet_to)
+output = telnet_handle.read_until("sername:", telnet_to)
+print output
+telnet_handle.write(username + '\n')
+output = telnet_handle.read_until('password:',telnet_to)
+print output
+telnet_handle.write(password + '\n')
+output = telnet_handle.read_very_eager()
+print output
+time.sleep(1)
+telnet_handle.write('term len 0' + '\n')
+time.sleep(1)
+output = telnet_handle.read_very_eager()
+telnet_handle.write('show ip int brief'+ '\n')
+time.sleep(2)
 output = telnet_handle.read_very_eager()
 
 print output

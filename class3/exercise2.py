7db34427fe473bdf004ec6c83339a5ed62541310
Amiri Gonzalez <amiri.gonzalez@gmail.com>
2016-05-04 12:24:00 -0700

    completed exercise2.py and the output created the (2) .svg files

--- class3/exercise2.py
+++ class3/exercise2.py
@@ -1,6 +1,7 @@
 import pysnmp
 import snmp_helper
 import pygal
+import time
 
 
 username = 'pysnmp'
@@ -10,7 +11,7 @@ encrypt_key = 'galileo1'
 snmp_user = (username, auth_key, encrypt_key)
 
 device = '50.76.53.27'
-snmp_port = 8061
+snmp_port = 7961
 
 snmp_device = (device, snmp_port)
 
@@ -23,26 +24,94 @@ snmp_oids = (
 )
 
 minperhour = 60
+running = True
+
+inoctets = []
+outoctets = []
+inucast = []
+outucast = []
+
+baseinoctet = ''
+baseoutoctet = ''
+baseinucast = ''
+baseoutucast = ''
 
 
-while (minperhour > 0):
-    for des, each_oid in snmp_oids:
-        snmp_data = snmp_helper.snmp_get_oid_v3(snmp_device, snmp_user, oid=each_oid)
-        output = int(snmp_helper.snmp_extract(snmp_data)
-        if des == 'ifInOctests_fa4':
-            inoctets.append[output]
-        if des == 'ifInUnicastPkts_fa4':
-            inunicast.append[output]
-        if des == 'ifOutOctests_fa4':
-            outoctets.append[output]
-        if des == 'ifOutUcastPkts_fa4':
-            outucast.append[output]
+#Obtain Base Value of Interface to have a starting point
+for dev, snmp_oid in snmp_oids:
+    if dev.startswith('ifDes'):
+        continue
+    else:
+        snmp_data = snmp_helper.snmp_get_oid_v3(snmp_device,snmp_user,oid=snmp_oid)
+        if dev.startswith('ifInOctet'):
+            baseinoctet = int(snmp_helper.snmp_extract(snmp_data))
+            print "Baseinoctet: ", baseinoctet
+        elif dev.startswith('ifInUcast'):
+            baseinucast = int(snmp_helper.snmp_extract(snmp_data))
+            print "BaseInUCast: ", baseinucast
+        elif dev.startswith('ifOutOctet'):
+            baseoutoctet = int(snmp_helper.snmp_extract(snmp_data))
+            print "BaseOutOctet: ", baseoutoctet
+        elif dev.startswith('ifOutUcast'):
+            baseoutucast = int(snmp_helper.snmp_extract(snmp_data))
+            print "BaseOutUcast: ", baseoutucast
+
+
+while running == True:
+    time.sleep(300)
     minperhour -= 5
-    time.sleep(300) 
+    print minperhour, ' minutes left'
+    if minperhour > 0:
+        for dev, snmp_oid in snmp_oids:
+            if dev.startswith('ifDes'):
+                continue    
+            else:
+                snmp_data = snmp_helper.snmp_get_oid_v3(snmp_device, snmp_user, oid=snmp_oid)
+                output = int(snmp_helper.snmp_extract(snmp_data))
+                if dev.startswith('ifInOctets'):
+                    new = output - baseinoctet
+                    inoctets.append(new)
+                elif dev.startswith('ifInUnicast'):
+                    new = output - baseinucast
+                    inunicast.append(new)
+                elif dev.startswith('ifOutOctets'):
+                    new = output - baseoutoctet
+                    outoctets.append(new)
+                elif dev.startswith('ifOutUcast'):
+                    new = output - baseoutucast
+                    outucast.append(new)
+    
+    else:
+        running = False
+
+
+#Create a Chart of Type Line
+line_chartA = pygal.Line()
+#Chart Title
+line_chartA.title = "Input/Output Octets"
+#Chart X-Axis Labels
+line_chartA.x_labels = ['5','10','15','20','25','30','35','40','45','50','55','60']
+#Add the lines to the graphs
+line_chartA.add('InOctets',inoctets)
+line_chartA.add('OutOctets',outoctets)
+
+#Chart image file
+line_chartA.render_to_file('inoutoctets.svg')
 
 
 
+#Create a Chart of Type Line
+line_chartB = pygal.Line()
+#Chart Title
+line_chartB.title = "Input/Output Ucast"
+#Chart X-Axis Labels
+line_chartB.x_labels = ['5','10','15','20','25','30','35','40','45','50','55','60']
+#Add the lines to the graphs
+line_chartB.add('InOctets',inucast)
+line_chartB.add('OutOctets',outucast)
 
+#Chart image file
+line_chartB.render_to_file('inoutucast.svg')
 
-
+print '.svg files have been created'
 

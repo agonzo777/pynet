48c7896c43735dddd0264fe3f5ddf5fa85dd20e1
Amiri Gonzalez <amiri.gonzalez@gmail.com>
2016-06-04 11:48:20 -0700

    created exercise4

--- class9/mytest/world.py
+++ class9/mytest/world.py
@@ -13,3 +13,24 @@ def main():
 if __name__ == "__main__": main()
 
 
+
+class Myclass(object):
+
+
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+    def repeat(self):
+        return "{} {} ".format(self.a,self.b) * self.c
+
+
+
+
+#class MyChildClass (Myclass):
+    def negate(self):
+        return "No {} {} ".format(self.a, self.b) * self.c
+
+
+
+
